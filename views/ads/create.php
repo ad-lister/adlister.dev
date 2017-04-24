1d56b9b7150327955ed49899a28d72ed99f65ca6
Matt Valdez <mattvaldez01@gmail.com>
2017-04-24 02:05:27 -0500

    gave an id to use to style for tablet

--- views/ads/create.php
+++ views/ads/create.php
@@ -47,7 +47,7 @@ if(!empty($_POST)) {
  <h2 class="text-center">Create an Ad</h2>
 
 
- <div class="container">
+ <div class="container" id="create">
  	<div class="row">
  		<div class="col-md-4 col-md-offset-4">
  			<form method="post" enctype="multipart/form-data">
